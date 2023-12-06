c885bbefd8622d24037ea5004e824ae318792981
Sree <sreejitheyne@gmail.com>
2023-12-06 23:08:34 +0530

    python app changed to webapp

--- app.py
+++ app.py
@@ -1,87 +1,41 @@
-import tkinter as tk
-from tkinter import ttk, messagebox
+# app.py
+from flask import Flask, render_template, request, redirect, url_for
 import pandas as pd
 
-class ExcelForm:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Excel Form")
+app = Flask(__name__)
 
-        # Create DataFrame to store data
+class ExcelForm:
+    def __init__(self):
         self.df = pd.DataFrame(columns=["Name", "Age", "Email"])
 
-        # Create and set up the form
-        self.setup_form()
-
-    def setup_form(self):
-        # Label and Entry for Name
-        lbl_name = ttk.Label(self.root, text="Name:")
-        lbl_name.grid(row=0, column=0, padx=10, pady=10, sticky="e")
-        self.entry_name = ttk.Entry(self.root)
-        self.entry_name.grid(row=0, column=1, padx=10, pady=10)
-
-        # Label and Entry for Age
-        lbl_age = ttk.Label(self.root, text="Age:")
-        lbl_age.grid(row=1, column=0, padx=10, pady=10, sticky="e")
-        self.entry_age = ttk.Entry(self.root)
-        self.entry_age.grid(row=1, column=1, padx=10, pady=10)
-
-        # Label and Entry for Email
-        lbl_email = ttk.Label(self.root, text="Email:")
-        lbl_email.grid(row=2, column=0, padx=10, pady=10, sticky="e")
-        self.entry_email = ttk.Entry(self.root)
-        self.entry_email.grid(row=2, column=1, padx=10, pady=10)
-
-        # Save Button
-        save_button = ttk.Button(self.root, text="Save", command=self.save_data)
-        save_button.grid(row=3, column=0, columnspan=2, pady=20)
-
-        # View Data Button
-        view_button = ttk.Button(self.root, text="View Data", command=self.view_data)
-        view_button.grid(row=4, column=0, columnspan=2, pady=10)
-
-    def save_data(self):
-        # Get values from entries
-        name = self.entry_name.get()
-        age = self.entry_age.get()
-        email = self.entry_email.get()
-
-        # Validate non-empty fields
+    def save_data(self, name, age, email):
         if not name or not age or not email:
-            messagebox.showerror("Error", "All fields must be filled.")
-            return
+            return False
 
-        # Append data to DataFrame
         new_data = {"Name": name, "Age": age, "Email": email}
         self.df = self.df.append(new_data, ignore_index=True)
+        self.df.to_excel("data.xlsx", index=False)
+        return True
 
-        # Clear entry fields
-        self.entry_name.delete(0, tk.END)
-        self.entry_age.delete(0, tk.END)
-        self.entry_email.delete(0, tk.END)
+    def get_data(self):
+        return self.df
 
-        messagebox.showinfo("Success", "Data saved successfully.")
+excel_form = ExcelForm()
 
-    def view_data(self):
-        # Display DataFrame in a new window
-        view_window = tk.Toplevel(self.root)
-        view_window.title("View Data")
+@app.route('/')
+def index():
+    return render_template('index.html', data=excel_form.get_data())
 
-        tree = ttk.Treeview(view_window, columns=list(self.df.columns), show="headings")
+@app.route('/save', methods=['POST'])
+def save():
+    name = request.form['name']
+    age = request.form['age']
+    email = request.form['email']
 
-        # Set column headings
-        for col in self.df.columns:
-            tree.heading(col, text=col)
+    if excel_form.save_data(name, age, email):
+        return redirect(url_for('index'))
+    else:
+        return "Error: All fields must be filled."
 
-        # Insert data into the Treeview
-        for _, row in self.df.iterrows():
-            tree.insert("", "end", values=row.tolist())
-
-        tree.pack(expand=True, fill=tk.BOTH)
-
-# Create the main application window
-root = tk.Tk()
-app = ExcelForm(root)
-
-# Start the Tkinter event loop
-root.mainloop()
+if __name__ == '__main__':
+    app.run(debug=True)
